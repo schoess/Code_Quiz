6069b3b6a029aff5cc347c02af16b8b5909785ea
nick <schoessnick@gmail.com>
2020-04-21 10:42:05 -0500

    built skeleton html page

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var q1 = ["a", "b", "c", "d"]
+var q2 = ["a", "b", "c", "d"]
+var q3 = ["a", "b", "c", "d"]
+var q4 = ["a", "b", "c", "d"]
+var q5 = ["a", "b", "c", "d"]
+
+
+document.getElementById("start-button").onclick = startQuiz;
+
+function startQuiz() {
+    
+
+
+}
