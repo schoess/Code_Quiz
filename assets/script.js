69cf9efe357104eef174353668e28484949976b5
nick <schoessnick@gmail.com>
2020-04-23 11:17:36 -0500

    Built basic quiz functionality

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,106 @@
-var totalScore = 0
-var questionNumber = 0
-var questionsLine = $("#questionsLine");
-var answerssLine = $("#answersLine");
+var totalScore = 0, 
+	questionNumber = 0,
+	allQuestions = [{
+        question: "What do you call a variable with multiple boolean values?",
+        choices: ["variable", "object", "array", "let"],
+        correctAnswer: "array"
+    },
+    {
+        question: "A useful tool for debugging during development is_______.",
+        choices: ["wrench", "Chrome dev tools", "Visual Studio Code", "keyboard"],
+        correctAnswer: "Chrome dev tools"
+    },
+    {
+        question: "Where will yuou find most of the answers to the questions you will have in your coding career?",
+        choices: ["Teachers", "Coworkers", "User manual", "The Internet"],
+        correctAnswer: "The Internet"
+    },
+    {
+        question: "What should you do when using git before you push a project to the repository?",
+        choices: ["pull", "bop it", "save it", "close it"],
+        correctAnswer: "pull"
+    }
+];
 
-var allQuestions = [{
-    question: "Who is Prime Minister of the United Kingdom?",
-    choices: ["Tony Blair", "Gordon Brown", "Winston Churchill", "David Cameron"],
-    correctAnswer: "David Cameron"
-},
-{
-    question: "What is the capital city of Spain?",
-    choices: ["Barcelona", "London", "Madrid", "Lisbon"],
-    correctAnswer: "Madrid"
-},
-{
-    question: "How many strings does a guitar have?",
-    choices: ["Three", "Four", "Five", "Six"],
-    correctAnswer: "Six"
-},
-{
-    question: "What year did MTV launch?",
-    choices: ["1980", "1992", "1981", "1979"],
-    correctAnswer: "1981"
-}];
 
-function correctAnswer (i) {
-    totalScore ++;
+var mainContent = $('#mainContent'); 
+
+function correctGuess (i) { 
+    totalScore ++; 
     questionNumber ++;
 
     var updatePage = ['<div id="answerDiv">' +
-    '<h1>Correct!<h1>' +
-    '<h2>Total Score: ' + totalScore + '</h2></div>'
-]
-}
+        '<h1>Correct!<h1>' +
+        '<h2>Total Score: ' + totalScore + '</h2></div>'], // 
+    whereToPut = updatePage[0].length -6; 
+
+    if(totalScore < 4){
+        var whatToPut = '<button type="button" id="nextButton" class="btn btn-primary">Next Question</button>';
+
+
+        
+        updatePage = [updatePage.slice(0, whereToPut), whatToPut, updatePage.slice(whereToPut)].join('');
+
+        $('#mainContent').html(updatePage);
+
+        $('#nextButton').on('click', function() { 
+                question(questionNumber);
+        });
+    } else {
+        
+        var whatToPut = '<h1>Congratulations, you Win!</h1><button id="restartButton">Play Again</button>';
+
+        updatePage = [updatePage.slice(0, whereToPut), whatToPut, updatePage.slice(whereToPut)].join('');
+
+        $('#mainContent').html(updatePage);
+
+        $('#restartButton').on('click', function() {
+            questionNumber = 0;
+            totalScore = 0;
+            question(questionNumber);
+        });
+    }
+
+    
+};
+
+function incorrectGuess(i) {
+    
+    totalScore = 0;
+    questionNumber = 0;
+
+    var updatePage = ['<div id="answerDiv"></div>' +
+        '<h1>Wrong!<h1>' +
+        '<button id="restartButton">Restart</button>'
+        ];
+    
+    $('#mainContent').html(updatePage);
+
+    $('#restartButton').on('click', function() {
+        question(questionNumber);
+    });
+};
+
+function question(i) {
+    mainContent.html('<div id="questionDiv">' +
+        '<h1>Question ' + (i + 1) + '<h1>' +
+        '<h2>' + allQuestions[i].question + '</h2>' +
+        '<input type="radio" name="questionChoices" value="' + allQuestions[i].choices[0] + '" checked="yes">' + allQuestions[i].choices[0] + '</input>' +
+        '<input type="radio" name="questionChoices" value="' + allQuestions[i].choices[1] + '">' + allQuestions[i].choices[1] + '</input>' +
+        '<input type="radio" name="questionChoices" value="' + allQuestions[i].choices[2] + '">' + allQuestions[i].choices[2] + '</input>' +
+        '<input type="radio" name="questionChoices" value="' + allQuestions[i].choices[3] + '">' + allQuestions[i].choices[3] + '</input>' +
+        '<button type="button" class="btn btn-primary" id="submitButton">Submit</button>' + '</div>'
+        
+    );
+    
+
+    $('#submitButton').on('click', function() {
+        if($('input:radio[name=questionChoices]:checked').val() === allQuestions[i].correctAnswer && i < 4) {
+            correctGuess();
+        } else {
+            incorrectGuess();
+        }
+    });
+};
+
+question(questionNumber);
